e64e23e93de8eb206c2e077d8c974ba76e4e13b0
ddalynng <d.fangloy@gmail.com>
2025-01-09 23:21:22 +0100

    gallery details page

--- src/pages/GalleryPage/GalleryPage.jsx
+++ src/pages/GalleryPage/GalleryPage.jsx
@@ -1,60 +1,43 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-const GalleryDetailsPage = () => {
+const GalleryPage = () => {
   const { id } = useParams();
   const [oneGallery, setGallery] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
   const navigate = useNavigate();
 
   useEffect(() => {
     const fetchGalleryDetails = async () => {
       try {
-        // Fetch the gallery details based on the gallery ID
         const { data } = await axios.get(
           `${API_URL}/galleries/one-gallery/${id}`
         );
         setGallery(data);
       } catch (error) {
-        setError("Oops! Something went wrong. Please try again later!");
-      } finally {
-        setLoading(false);
+        console.log(error);
       }
     };
 
     fetchGalleryDetails();
   }, [id]);
 
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-
-  if (error) {
-    return <p>{error}</p>;
-  }
-
-  if (!gallery) {
-    return <p>Gallery not found!</p>;
+  if (!oneGallery) {
+    return <p>Loading gallery details...</p>;
   }
 
   return (
     <div className="gallery-details">
       <h2>{oneGallery.name}</h2>
-      <p>Description: {oneGallery.description}</p>
-      {/* If gallery has an image, display it */}
-      {oneGallery.images && (
-        <img
-          src={`${API_URL}/images/${oneGallery.images}`}
-          alt={oneGallery.name}
-        />
+      <p> {oneGallery.description}</p>
+      {oneGallery.images && oneGallery.images.length > 0 && (
+        <img src={oneGallery.images[0]} alt={oneGallery.name} />
       )}
-      <button onClick={() => navigate("/galleries")}></button>
+      <button onClick={() => navigate("/")}>Back to Galleries</button>
     </div>
   );
 };
 
-export default GalleryDetailsPage;
+export default GalleryPage;
